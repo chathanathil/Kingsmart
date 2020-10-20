777c0d0a81ea0d873f3640c46699277aaa9ef6d6
MOHAMMED FAHAD C <36698410+chathanathil@users.noreply.github.com>
2020-10-20 07:04:38 +0530

    db connected

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "localhost";
-$db_name = "Kings-mart";
-$username = "root";
-$password = "";
+$host = "f2fbe0zvg9j8p9ng.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_name = "vwadnxej7bnryorz";
+$username = "rse65xjrvs8quadw";
+$password = "ovfwk8go75f2rkbs";
   
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
